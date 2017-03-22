dd6844751e4ec659c21f5ea74aca7cd662f2adfc
aleccalvo08 <aleccalvo@Alecs-MacBook-Pro.local>
2017-03-22 18:06:36 -0400

    Changes at Mar 22 at Work

--- js/main.js
+++ js/main.js
@@ -4,17 +4,40 @@ $(function() {
 
   $('.main__nav__overlay-close').click(closeNav)
 
+  $('.main__nav__overlay-items').click(closeNav)
+
+  $('.main__video-text').click(function() {
+    $('.main__video__overlay').addClass('overlay-show')
+    $('#videoJones').get(0).play()
+    noScroll()
+  })
+
+  $('.main__video__overlay').click(function() {
+    $(this).removeClass('overlay-show')
+    $('#videoJones').get(0).pause()
+    withScroll()
+  })
+
   function openNav() {
-    $('#myNav').addClass('main__nav__overlay-show')
+    $('#myNav').addClass('overlay-show')
   }
 
   function closeNav() {
-    $('#myNav').removeClass('main__nav__overlay-show')
+    $('#myNav').removeClass('overlay-show')
   }
 
-  $('.main__video-text').click(function() {
-    $('.main__video').show()
-  })
+  function noScroll() {
+    $('body').addClass('no-scroll')
+  }
 
+  function withScroll() {
+    $('body').removeClass('no-scroll')
+  }
+
+  $('.main__scroll-down').click(function() {
+    $('html, body').animate({
+        scrollTop: $(".about-us").offset().top - 20
+    }, 1000)
+  })
 
 })
